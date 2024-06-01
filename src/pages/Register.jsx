34befa324f7e31b5080a686bf1f65a9c0d68ec01
abtaheetaseen <abtahee.siddiqui@northsouth.edu>
2024-06-01 15:42:49 +0600

    third commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,66 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../provider/AuthProvider';
 import toast from 'react-hot-toast';
 import { Helmet } from 'react-helmet-async';
-import useAxiosPublic from '../hooks/useAxiosPublic';
 import { imageUpload } from '../imageAPI';
-import { updateProfile } from 'firebase/auth';
+import useAxiosSecure from '../hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosPublic from '../hooks/useAxiosPublic';
 
 const Register = () => {
+
+    const [selectDivision, setSelectDivision] = useState("");
+    const [selectDistrict, setSelectDistrict] = useState("");
+    const [selectUpazila, setSelectUpazila] = useState("");
+    const [selectBloodGroup, setSelectBloodGroup] = useState("");
 
     const { createUser, updateUserProfile } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
     const from = location?.state || "/";
-    // const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
+    const axiosPublic = useAxiosPublic();
+
+    const handleDivision = (e) => {
+        setSelectDivision(e.target.value);
+    }
+
+    const handleDistrict = (e) => {
+        setSelectDistrict(e.target.value);
+    }
+
+    const handleUpazila = (e) => {
+        setSelectUpazila(e.target.value);
+    }
+
+    const handleBloodGroup = (e) => {
+        setSelectBloodGroup(e.target.value);
+    }
+
+    const {data: divisions = []} = useQuery({
+        queryKey: ["divisions"],
+        queryFn: async() => {
+            const res = await axiosPublic.get("/divisions")
+            return res.data;
+        }
+    })
+
+    const {data: districts = []} = useQuery({
+        queryKey: ["districts"],
+        queryFn: async() => {
+            const res = await axiosPublic.get("/districts")
+            return res.data;
+        }
+    })
+
+    const {data: upazilas = []} = useQuery({
+        queryKey: ["upazilas"],
+        queryFn: async() => {
+            const res = await axiosPublic.get("/upazilas")
+            return res.data;
+        }
+    })
 
     const handleRegister = async (e) => {
         e.preventDefault();
@@ -23,6 +70,10 @@ const Register = () => {
         const password = form.password.value;
         const confirmPassword = form.confirmPassword.value;
         const image = form.image.files[0];
+        const division = selectDivision;
+        const district = selectDistrict;
+        const upazila = selectUpazila;
+        const bloodGroup = selectBloodGroup;
 
         try {
 
@@ -48,6 +99,25 @@ const Register = () => {
             // update profile
             await updateUserProfile(name, image_url)
 
+            // send user data to mongodb
+
+            const user = {
+                name,
+                email,
+                image_url,
+                division,
+                district,
+                upazila,
+                bloodGroup
+            }
+
+            const res = await axiosSecure.post("/users", user)
+            console.log(res.data)
+            if(res.data.insertedId){
+                console.log("User created")
+            }
+            
+
             toast.success("User registered successfully");
             navigate(from);
 
@@ -58,76 +128,10 @@ const Register = () => {
 
     }
 
-    // const handleRegister = (e) => {
-    //     e.preventDefault();
-    //     const form = e.target;
-    //     const name = form.name.value;
-    //     const image = form.image.files[0];
-    //     const email = form.email.value;
-    //     const password = form.password.value;
-    //     const confirmPassword = form.confirmPassword.value;
-
-    //     pass validation
-    //     if (!/[A-Z]/.test(password)) {
-    //         toast.error("Password must have at least one capital letter");
-    //         return;
-    //     } else if (!/[a-z]/.test(password)) {
-    //         toast.error("Password must have at least one small letter");
-    //         return;
-    //     } else if (password !== confirmPassword) {
-    //         toast.error("Password and Confirm Password is not matched");
-    //         return;
-    //     }
-
-
-    //      upload image and get image url
-    //      const image_url = imageUpload(image);
-
-    //     const user = {
-    //         name,
-    //         image_url,
-    //         email,
-    //     }
-
-    //     console.log(user)
-
-
-    //     create user
-    //     createUser(email, password)
-    //         .then((result) => {
-    //             console.log(result.user);
-    //             form.reset();
-    //             toast.success("User created successfully");
-    //             navigate(from);
-
-    //             update profile
-    //             updateProfile(result.user, {
-    //                 displayName: name,
-    //                 photoURL: image_url
-    //             })
-    //                 .then(() => console.log("profile updated"))
-    //                 .catch(error => {
-    //                     console.log(error.message)
-    //                 })
-
-    //              send to backend server
-    //             axiosPublic.post("/users", user)
-    //                 .then(res => {
-    //                     console.log(res.data)
-    //                 })
-
-
-    //         })
-    //         .catch((error) => {
-    //             console.log(error)
-    //             toast.error(error.message)
-    //         });
-    // }
-
     return (
         <>
             <Helmet>
-                <title>Chef-Cuisine || Register</title>
+                <title>LIFE-DROP | REGISTER</title>
             </Helmet>
 
             <section className="bg-white min-h-screen">
@@ -149,19 +153,6 @@ const Register = () => {
                             <input type="text" name='name' className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-red-400 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Your Name" required />
                         </div>
 
-                        {/* <div>
-                            <label htmlFor='image' className='block mb-2 text-sm'>
-                                Select Image:
-                            </label>
-                            <input
-                                required
-                                type='file'
-                                id='image'
-                                name='image'
-                                accept='image/*'
-                            />
-                        </div> */}
-
                         <div className="relative flex items-center mt-8">
                             <span className="absolute">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500">
@@ -178,6 +169,53 @@ const Register = () => {
                                 name='image'
                                 accept='image/*' className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-red-400 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
+
+                        <div className='mb-5 mt-4'>
+                    <label className="text-red-600" htmlFor="divisions">Division</label>
+                    <select value={selectDivision} onChange={handleDivision} className="select select-bordered block w-full py-3 text-gray-700 bg-white border rounded-lg  dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-red-400 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                        {
+                            divisions.map(division => <option key={division._id}>
+                                {division.division_name}
+                            </option>)
+                        }
+                    </select>
+                    </div>
+
+                    <div className='mb-5 mt-4'>
+                    <label className="text-red-600" htmlFor="districts">District</label>
+                    <select value={selectDistrict} onChange={handleDistrict} className="select select-bordered block w-full py-3 text-gray-700 bg-white border rounded-lg  dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-red-400 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                        {
+                            districts.map(district => <option key={district._id}>
+                                {district.name}
+                            </option>)
+                        }
+                    </select>
+                    </div>
+
+                    <div className='mb-5 mt-4'>
+                    <label className="text-red-600" htmlFor="upazilas">Upazila</label>
+                    <select value={selectUpazila} onChange={handleUpazila} className="select select-bordered block w-full py-3 text-gray-700 bg-white border rounded-lg  dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-red-400 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                        {
+                            upazilas.map(upazila => <option key={upazila._id}>
+                                {upazila.name}
+                            </option>)
+                        }
+                    </select>
+                    </div>
+
+                    <div className='mb-5 mt-4'>
+                    <label className="text-red-600" htmlFor="bloodGroup">Blood Group</label>
+                    <select value={selectBloodGroup} onChange={handleBloodGroup} className="select select-bordered block w-full py-3 text-gray-700 bg-white border rounded-lg  dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-red-400 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                        <option>A+</option>
+                        <option>A-</option>
+                        <option>B+</option>
+                        <option>B-</option>
+                        <option>AB+</option>
+                        <option>AB-</option>
+                        <option>O+</option>
+                        <option>O-</option>
+                    </select>
+                    </div>
 
                         <div className="relative flex items-center mt-4">
                             <span className="absolute">
